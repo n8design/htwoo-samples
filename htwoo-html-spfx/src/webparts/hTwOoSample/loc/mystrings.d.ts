23e217e5696e01e043d6b89a27ed09a213e0347b
Stefan Bauer <office@n8d.at>
2021-04-25 16:13:45 +0200

    No Framework Sample Added

---
+++ htwoo-html-spfx/src/webparts/hTwOoSample/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHTwOoSampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HTwOoSampleWebPartStrings' {
+  const strings: IHTwOoSampleWebPartStrings;
+  export = strings;
+}
